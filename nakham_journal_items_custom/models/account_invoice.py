ecda48220041c3756feeaadcfea08dbae1987f3d
Thomas Edward <s.safwat@centione.com>
2021-01-01 01:51:04 +0200

    nakham_journal_operation_rules d

--- nakham_journal_items_custom/models/account_invoice.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, models, fields
-from odoo.osv import osv
-import odoo.addons.decimal_precision as dp
-
-
-class AccountInvoice(models.Model):
-    _inherit = "account.move.line"
-
-    group_id = fields.Many2one(related="analytic_account_id.group_id",comodel_name="account.analytic.group", string="Group",store=True, required=False, )
