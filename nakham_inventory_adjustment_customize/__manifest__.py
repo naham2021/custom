6caaf9482c8290df78daa39be099848445d9d577
Thomas Edward <s.safwat@centione.com>
2021-01-03 15:05:04 +0200

    add nakham_inventory_adjustment_customize

---
+++ nakham_inventory_adjustment_customize/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "nakham_inventory_adjustment_customize",
+    'author': "Centione",
+    'website': "http://www.centione.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'stock', 'stock_account', 'account','nakham_analytic_account'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/views.xml',
+        'data/data.xml',
+    ]
+}
