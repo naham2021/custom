2357853fce5f1bd3ab76f4ce2c47984c55d0c0ec
Mahmoud ALaa <m.alaa@centione.com>
2021-01-31 10:58:41 +0200

    update for hr

---
+++ nakham_hr_customizations/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "nakham_hr_customizations",
+
+    'summary': """
+        """,
+
+    'description': """
+    """,
+
+    'author': "Centione",
+    'website': "www.centione.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/hr_contract_inherit.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
