b6e77a479e11cde7598a944774fee13f582efe52
Mahmoud ALaa <m.alaa@centione.com>
2021-01-31 13:40:51 +0200

    update for hr

--- nakham_hr_customizations/__manifest__.py
+++ nakham_hr_customizations/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr'],
+    'depends': ['base','hr_contract'],
 
     # always loaded
     'data': [
