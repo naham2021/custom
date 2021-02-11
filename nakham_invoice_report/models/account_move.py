f45e7181b07af381bfe5c44dcab1d9c2cd7ea651
Beshoy Nabeih <beshoybosha231@gmail.com>
2021-02-11 12:33:36 +0200

    [mod]nakham_invoice_report

--- nakham_invoice_report/models/account_move.py
+++ nakham_invoice_report/models/account_move.py
@@ -3,17 +3,15 @@
 from odoo import models, fields, api
 
 
-class AccounrMove(models.Model):
-    _inherit = 'account.move.line'
+class AccountMoveInherit(models.Model):
+    _inherit = 'account.move'
 
-    tax_value = fields.Float(string="Tax Value",  required=False,compute="_compute_tax_value" )
-    def _compute_tax_value(self):
+    @api.depends('invoice_line_ids.discount', 'invoice_line_ids.price_unit', 'invoice_line_ids.quantity')
+    def _cal_total_discount(self):
         for rec in self:
-            tota_amount = 0
-            for tax in rec.tax_ids:
-                tota_amount += tax.amount
-
-            if tota_amount > 0 :
-                rec.tax_value = rec.price_subtotal * (tota_amount /100)
-            else:
-                rec.tax_value = 0
+            total_discount = 0.0
+            for line in rec.invoice_line_ids:
+                total_discount += round(line.price_unit * line.quantity * line.discount / 100, 2)
+            print(total_discount)
+            rec.total_discount = total_discount
+    total_discount = fields.Monetary(compute="_cal_total_discount")
