d0796c5176dfce1b01a5ac96263b9abe29a23306
Mahmoud ALaa <m.alaa@centione.com>
2021-02-17 16:56:55 +0200

    hr analytic account of contract on pasy slips

--- naham_contract_analy_acc_in_journal_for_payslips/__manifest__.py
+++ naham_contract_analy_acc_in_journal_for_payslips/__manifest__.py
@@ -22,9 +22,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/db_credentials.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
