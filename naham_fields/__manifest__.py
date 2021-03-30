a5926c572ec046b5dead5ee5a84eda404208f4f7
naham2021 <beshoybosha231@gmail.com>
2021-03-30 18:34:03 +0200

    naham_fields

--- naham_fields/__manifest__.py
+++ naham_fields/__manifest__.py
@@ -27,7 +27,8 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/project.xml'
+        'views/project.xml',
+        'reports/naham_report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
