a67549a4eddf969466dad304ffe50743f140403b
kerolesayed <kerolesayed@gmail.com>
2022-06-29 14:36:47 +0200

    category product

--- nakham_journal_items_custom/models/account_invoice.py
+++ nakham_journal_items_custom/models/account_invoice.py
@@ -5,4 +5,4 @@ class AccountInvoice(models.Model):
     _inherit = "account.move.line"
 
     group_id = fields.Many2one(related="analytic_account_id.group_id",comodel_name="account.analytic.group", string="Group",store=True, required=False, )
-    product_categ = fields.Many2one(related="product_id.categ_id",comodel_name="product.category", string="Product Category",store=True, required=False, )
+    product_categ = fields.Many2one(related="product_id.categ_id",comodel_name="product.category", string="Product Category", required=False, )
