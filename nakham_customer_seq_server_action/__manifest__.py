637a677d6c4895b4d11509bc22fa6f75fab2834b
toqa <toqa@centione.com>
2022-10-13 13:51:53 +0200

    seq server action and reports

---
+++ nakham_customer_seq_server_action/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "nakham_customer_seq_server_action",
+    'author': "Centione",
+    'website': "",
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+
+}
